017795fd44d0a267ec0207be52ec54c07623c5a4
Pablo Paglilla <pablopaglilla16@gmail.com>
2019-02-21 04:18:48 -0300

    Agregar primera implementación del server concurrente. Falta testear.

--- sockets.c
+++ sockets.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+
+#include "sockets.h"
 
 int get_local_addrinfo(const char* port, struct addrinfo* hints, struct addrinfo** server_info) {
 
@@ -154,4 +159,134 @@ int create_socket_client(const char* host, const char* port) {
 	freeaddrinfo(server_info);
 
 	return socket_fd;
+}
+
+int run_handler(handler_t handler, int socket_fd) {
+	if(handler != NULL) {
+		return handler(socket_fd);
+	}
+	return -1;
+}
+
+struct clients_storage {
+	int* clients;
+	int numClients;
+	int maxClients;
+};
+
+struct clients_storage init_clients_storage() {
+	struct clients_storage storage;
+	storage.clients = NULL;
+	storage.numClients = 0;
+	storage.maxClients = 0;
+	return storage;
+}
+
+#define MAX_EPOLL_EVENTS 10
+
+int thread_should_stop(struct server_mutex* mutex) {
+	pthread_mutex_lock(&mutex->lock);
+	int stop = mutex->shouldStop;
+	pthread_mutex_unlock(&mutex->lock);
+	printf("Should stop: %d\n", stop);
+	return stop;
+}
+
+int add_client(struct clients_storage* storage, int client) {
+	if(!(storage->numClients < storage->maxClients)) {
+		int* new_buf = realloc(storage->clients, storage->maxClients * 2);
+		if (new_buf == NULL) {
+			return -1;
+		}
+		storage->clients = new_buf;
+		storage->maxClients *= 2;
+	}
+	storage->clients[storage->numClients] = client;
+	storage->numClients++;
+	return 0;
+}
+
+void remove_client(struct clients_storage* storage, int client) {
+	for(int i = 0; i < storage->numClients; i++) {
+		if(client == storage->clients[i]) {
+			for(int j = i + 1; j < storage->numClients; j++) {
+				storage->clients[j - 1] = storage->clients[j];
+			}
+			break;
+		}
+	}
+	storage->numClients--;
+}
+
+void clear_clients(struct clients_storage storage) {
+	for(int i = 0; i < storage.numClients; i++) {
+		close(storage.clients[i]);
+	}
+	free(storage.clients);
+}
+
+int add_epoll_fd(int epoll_fd, int socket_fd) {
+	struct epoll_event event;
+	event.events = EPOLLIN;
+	event.data.fd = socket_fd;
+	if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event) == -1) {
+		fprintf(stderr, "Couldn't add file descriptor to epoll. Errno: %d\n", errno);
+		return -1;
+	}
+	return 0;
+}
+
+int remove_epoll_fd(int epoll_fd, int socket_fd) {
+	if(epoll_ctl(epoll_fd, EPOLL_CTL_DEL, socket_fd, NULL) == -1) {
+		fprintf(stderr, "Couldn't remove file descriptor to epoll. Errno: %d\n", errno);
+		return -1;
+	}
+	return 0;
+}
+
+void* run_server(void * input) {
+
+	struct server_input* data = (struct server_input*) input;
+	struct epoll_event events[MAX_EPOLL_EVENTS];
+	struct clients_storage storage = init_clients_storage();
+	struct sockaddr_storage client_addr;
+	socklen_t sin_size = sizeof client_addr;
+	int epoll_event_count;
+	int epoll_fd = epoll_create1(0);
+
+	if(epoll_fd == -1) {
+		fprintf(stderr, "Couldn't get epoll file descriptor. Errno: %d\n", errno);
+	}
+
+	if(add_epoll_fd(epoll_fd, data->server_fd) == -1) {
+		close(epoll_fd);
+		return 0;
+	}
+
+	while(!thread_should_stop(&data->mutex)) {
+		epoll_event_count = epoll_wait(epoll_fd, events, 
+			MAX_EPOLL_EVENTS, 1000);
+		for(int i = 0; i < epoll_event_count; i++) { 
+			int socket_fd = events[i].data.fd;
+			printf("Socket: %d\n", socket_fd);
+			if(socket_fd == data->server_fd) {
+				int new_client =  accept(socket_fd, (struct sockaddr*) &client_addr, &sin_size);
+				if(run_handler(data->handlers.on_new_client, new_client) == CLOSE_CLIENT) {
+					close(new_client);
+				} else {
+					add_client(&storage, new_client);
+					add_epoll_fd(epoll_fd, new_client);
+				}
+			} else {
+				if(run_handler(data->handlers.on_can_read, socket_fd) == CLOSE_CLIENT) {
+					remove_client(&storage, socket_fd);
+					remove_epoll_fd(epoll_fd, socket_fd);
+					close(socket_fd);
+				}
+			}
+		}
+	}
+
+	clear_clients(storage);
+	close(epoll_fd);
 }
--- sockets.h
+++ sockets.h
@@ -5,4 +5,26 @@ int create_socket_server(const char*, int);
 
 int create_socket_client(const char*, const char *);
 
+void* run_server(void*);
+
+typedef int (*handler_t)(int);
+
+enum handler_return { CLOSE_CLIENT = 10 };
+
+struct handler_set {
+	handler_t on_new_client;
+	handler_t on_can_read;
+};
+
+struct server_mutex {
+	pthread_mutex_t lock;
+	int shouldStop;
+};
+
+struct server_input {
+	struct server_mutex mutex;
+	int server_fd;
+	struct handler_set handlers;
+};
+
 #endif
